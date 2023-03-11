8e2e80021fdabd3f2d1cbda7f00aa48df1571d65
javohir <127494412+JavohirMirzo05@users.noreply.github.com>
2023-03-11 15:05:49 +0500

    1_uy.ishi_py

---
+++ 1 uy ishi.py
@@ -0,0 +1,49 @@
+
+# a=9
+# m=8
+# j=a+m
+# print(j)
+
+# f=5
+# d=3
+# j=f-d
+# print(j)
+
+# h=3
+# k=9
+# j=h * f
+# print(j)
+
+# y=10
+# i=2
+# j=y/i
+# print(j)
+
+# d=8
+# f=3
+# j=d%f
+# print(j)
+
+# k=40
+# l=2
+# j=k//l
+# print(j)
+
+# a=287
+# s=37
+# j=a==s
+# print(j)
+
+# a=78
+# p=63
+# print(a<p)
+
+# d=4837
+# s=4937
+# print(d>s)
+
+# a=10
+# b=3
+# print(a**b)
+
+
